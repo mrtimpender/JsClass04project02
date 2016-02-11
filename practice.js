461a43a81ce1b2e6385e663291237dc57c8ac119
mrtimpender <mrtimpender@gmail.com>
2016-02-10 19:02:52 -0700

    commit2

--- practice.js
+++ practice.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 ​
+// anything
 
 // 1. When 'Submit' is clicked, add the contents of the textbox below the button in a h3 element.
 
 
-// Add ( and label) your jQuery code here. 
+// Add ( and label) your jQuery code here.
 
 ​
 });
